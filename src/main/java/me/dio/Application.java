19821a4c76fc83382def29e439076531fb2c0845
genisonleal <genisonleal@gmail.com>
2025-07-11 20:35:14 -0300

    Commit inicial do projeto

---
+++ src/main/java/me/dio/Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDev2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDev2025Application.class, args);
+	}
+
+}
